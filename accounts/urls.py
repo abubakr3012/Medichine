1714967b36999c90b8801be23fde076dd376688a
Abubakr <anon.world.tj@gmail.com>
2026-05-25 14:18:16 +0300

    17

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,7 +9,6 @@ urlpatterns = [
     path('forget_password',views.forget_password,name='forget_password'),
     path('reset_confirm',views.reset_confirm,name='reset_confirm'),
     path("dashboard/",views.redirect_dashboard,name="redirect_dashboard"),    
-    path('dashboard/',    views.redirect_dashboard,name='redirect_dashboard'),
     path('patient/dashboard/',views.patient_dashboard,name='patient_dashboard'),
     path('doctor/dashboard/',views.doctor_dashboard,name='doctor_dashboard'),
     path('admin/dashboard/',views.admin_dashboard,name='admin_dashboard'),
