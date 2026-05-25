168d9fec55a83333901886e0c1dc00cd8e2d45a1
Abubakr <anon.world.tj@gmail.com>
2026-05-25 22:49:41 +0300

    26

--- appointments/models.py
+++ appointments/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+User=settings.AUTH_USER_MODEL
+
+class Appointment(models.Model):
+    patient = models.ForeignKey(User,on_delete=models.CASCADE,related_name='appointments')
+    doctor=models.ForeignKey('doctors.DoctorProfile',on_delete=models.CASCADE,related_name='appointments')
+    date=models.DateTimeField(auto_now_add=True)
+
+    STATUS = (
+        ('pending', 'Ожидание'),
+        ('approved', 'Подтверждена'),
+        ('cancelled', 'Отменена'),
+    )
+
+    status=models.CharField(max_length=30,choices=STATUS,default='pending')
+    created_at=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.patient.username}:{self.doctor.username}'
