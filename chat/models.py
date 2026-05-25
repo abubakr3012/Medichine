de9e1a9f5a225b2774e8a7057ca6c601a53029fc
Abubakr <anon.world.tj@gmail.com>
2026-05-25 14:27:04 +0300

    18

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Message(models.Model):
+    user=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    text=models.TextField()
+    writed_at=models.DateTimeField(auto_now_add=True)
+    likes=models.IntegerField(default=0)
+
+    def __str__(self):
+        return f'{self.user.username}:{self.text[:20]}'
