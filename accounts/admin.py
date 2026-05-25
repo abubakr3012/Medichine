5aa6250f32cbcc1e502b516ea63c0756e2f60899
Abubakr <anon.world.tj@gmail.com>
2026-05-25 13:16:53 +0300

    11

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register([EmailConfirm,ResetPassword])
+admin.site.register([EmailConfirm,ResetPassword,Profile,User])
--- accounts/models.py
+++ accounts/models.py
@@ -37,3 +37,23 @@ class ResetPassword(models.Model):
 
     def __str__(self):
         return self.user.username
+    
+
+class Profile(models.Model):
+
+    ROLE_CHOICES = (
+        ('patient', 'Patient'),
+        ('doctor', 'Doctor'),
+        ('admin', 'Admin'),
+    )
+
+    user=models.OneToOneField(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,related_name='profile')
+    age=models.PositiveIntegerField(null=True,blank=True)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='patient')
+    phone=models.CharField(max_length=14,blank=True)
+    photo=models.ImageField(upload_to='users/',blank=True,null=True)
+
+
+    def __str__(self):
+        return f'{self.user}:{self.phone}'
+
