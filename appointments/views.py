ce17aa6bafe2d7241c646f4520d13a8a46321c1b
Abubakr <anon.world.tj@gmail.com>
2026-05-28 09:00:57 +0300

    48

--- appointments/views.py
+++ appointments/views.py
@@ -12,3 +12,7 @@ def create_appointment(request,pk):
         )
         return redirect('doctors_list')
     return render(request,'appoinments/appoinments.html',{"doctor":doctor})
+
+def show_appointment(request):
+    appointment=Appointment.objects.all()
+    return render(request,'appointments/show_appointment.html',{"appointments":appointment})
