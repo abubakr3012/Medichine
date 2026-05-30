8dfa1119170779d528afa952d2a78d9e228af6ce
Abubakr <anon.world.tj@gmail.com>
2026-05-30 14:44:21 +0300

    91

--- chat/models.py
+++ chat/models.py
@@ -13,7 +13,6 @@ class Message(models.Model):
     def __str__(self):
         return f'{self.user.username}:{self.text[:20]}'
     
-
 class Direct(models.Model):
     sender=models.ForeignKey(User,on_delete=models.CASCADE,related_name='send_messages')
     receiner=models.ForeignKey(User,on_delete=models.CASCADE,related_name='received_messages')
