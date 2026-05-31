8763ed5de53961cc4bf194a1e972bcb7b01b8875
Abubakr <anon.world.tj@gmail.com>
2026-05-31 22:58:12 +0300

    detail doctor

--- doctors/urls.py
+++ doctors/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('doctors/',views.show_all_doctors,name='doctors_list')
+    path('doctors/',views.show_all_doctors,name='doctors_list'),
+    path('detail/<int:pk>',views.DoctorDetailView.as_view(),name='detail')
 ]
--- doctors/views.py
+++ doctors/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render,redirect,get_object_or_404
 from .models import DoctorProfile
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q,Max,Count
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import generic
 
 @login_required(login_url='login')
 def show_all_doctors(request):
@@ -17,3 +19,11 @@ def show_all_doctors(request):
         all_patients=Count('id')
     )
     return render(request,'doctors/doctors.html',{"doctors":profile,"stats":stats})
+
+class DoctorDetailView(LoginRequiredMixin,generic.DetailView):
+    model=DoctorProfile
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['is_owner'] = self.request.user == self.get_object().user
+        return context
